921cf5396b84ae3086c59f15faeb5f15fd52d641
indri <indri111006@gmail.com>
2024-02-27 13:56:35 +0700

    selesai

---
+++ resources/views/admin/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+    All Rights Reserved by SMK Al Hafidz Leuwiliang. Designed and Developed by <a href="https://instagram.com/stinuraisyah11" target="_blank">Siti Nur Aisyah</a>.
+</footer>
